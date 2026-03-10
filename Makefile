f6a5e4ec42a72bd03e71c0fdc47b7101013fed52
Tushar Rawat <tusharrawat.trf@gmail.com>
2026-03-10 10:28:05 +0530

    Project init

---
+++ Makefile
@@ -0,0 +1,5 @@
+build: 
+	g++ main.cpp -o ./bin/a.out
+
+run:
+	./bin/a.out
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int Logger() {
+  std::cout << "Trading Engine Project Init";
+  return 0;
+}
+
+int main() {
+  Logger();
+  return 0;
+}
